b829cb9280d917016d8aa83d68ddc2a23633ee36
Leo Chen <leo@saltedherring.com>
2016-05-30 11:31:13 -1200

    added the annotation

--- src/is_above_viewport.js
+++ src/is_above_viewport.js
@@ -1,3 +1,13 @@
+/**
+ * @file is_above_viewport.js
+ *
+ * test an element is above the viewport
+ * */
+ 
+/**
+ * - el: The element that you want to test
+ * - offset: the offset the element is away from the top edge of the screen
+ * */
 (function($) {
 
 	$.fn.isAboveViewport = function(el, offset) {

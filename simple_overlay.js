bc46dda08c50f88dd5972331695620746c8c527f
Leo Chen <leo@saltedherring.com>
2016-05-29 17:58:21 -1200

    ajax loader for lazy load, simple_overlay for message popup, and utitlies is for a bunch of handy functions

---
+++ simple_overlay.js
@@ -0,0 +1,78 @@
+var simplayer = function(title,content,buttons,zindex,maxWidth) {
+	this.tray			=	$('<div />').attr('id','simplayer-tray');
+	this.title			=	$('<h2 />').addClass('simplayer-title').html(title);
+	this.content			=	$('<div />').addClass('simplayer-content').html(content);
+	this.buttons			=	$('<div />').addClass('clearfix simplayer-buttons');
+	
+	var _self			=	this,
+		_tray			=	this.tray,
+		_wrapper		=	null,
+		_thisButtons 	=	this.buttons,
+		_maxWidth		=	maxWidth;
+	
+	
+	if (buttons && typeof(buttons) == 'object' && buttons.length > 0) {
+		buttons.forEach(function(btn) {
+			var sbtn = $('<button />').addClass('simplayer-button button').html(btn.Label);
+			if (btn.Event && typeof(btn.Event) == 'function') {
+				sbtn.click(btn.Event);
+			}
+			_thisButtons.append(sbtn);
+		});
+	}else{
+		var sbtn = $('<button />').addClass('simplayer-button button').html('OK');
+		sbtn.click(function(e) {
+            _self.close();
+        });
+		_thisButtons.append(sbtn);
+	}
+	
+	this.wrapper			=	$('<div />').attr('id', 'simplayer-wrapper').append(this.title, this.content, this.buttons);
+	
+	if (_maxWidth) {
+		this.wrapper.css('max-width', _maxWidth);
+	}
+	
+	_wrapper = this.wrapper;
+	
+	if (zindex) {
+		this.tray.css('z-index', zindex);
+		this.wrapper	.css('z-index', zindex+1);
+	}
+	this.show 			=	function() {
+		$('body').css('overflow','hidden').append(_tray, _wrapper);
+		
+		_wrapper.css('max-height', '90%');
+		var wrapperHeight	=	_wrapper.outerHeight(),
+			wrapperPadding	=	_wrapper.css('padding-top').replace(/px/gi, '').toFloat() + _wrapper.css('padding-bottom').replace(/px/gi, '').toFloat(),
+			margins			=	_wrapper.find('.simplayer-title').margin('vertical') + _wrapper.find('.simplayer-buttons').margin('vertical'),
+			nonCntHeight	=	_wrapper.find('.simplayer-title').outerHeight() + _wrapper.find('.simplayer-buttons').outerHeight();
+		
+		_wrapper.find('.simplayer-content').css('max-height', wrapperHeight - nonCntHeight - margins - wrapperPadding).css('overflow-y','auto');
+		TweenMax.to(_wrapper, 0, {opacity: 0, scale: 0, onComplete:function() {
+			TweenMax.to(_wrapper, 0.25, {opacity: 1, scale: 1, ease: Back.easeOut.config(1.7)});
+		}});
+	};
+	
+	
+	this.close			=	function() {
+		TweenMax.to(_wrapper, 0.25, {opacity: 0, scale: 0, ease: Back.easeIn.config(1.7), onComplete:function() {
+			_wrapper.remove();
+			_tray.remove();
+			$('body').removeAttr('style');
+			
+			delete _self.tray;
+			delete _self.title;
+			delete _self.content;
+			delete _self.buttons;
+			delete _self.wrapper;
+		}});
+	};
+	
+	this.btnEvent		=	function(idx, event) {
+		var buttons = this.buttons.find('.simplayer-button');
+		if (idx < buttons.length) {
+			buttons.eq(idx).unbind('click').click(event);
+		}
+	};
+};

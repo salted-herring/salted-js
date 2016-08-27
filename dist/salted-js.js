da030ac1aaa34665f422bea1be2497fe3caeebe3
Leo Chen <leo@saltedherring.com>
2016-08-26 22:14:37 -1200

    added ajax queue and ajaxrequest

--- dist/salted-js.js
+++ dist/salted-js.js
@@ -101,6 +101,41 @@ var ajaxLoader = function(elementSelector, linkSelector, destination, preprocess
 	};
 	
 	return this;
+};;var ajaq = function() {
+	this.queue = [];
+	var self = this;
+	
+	this.add = function(ajaxRequest) {
+		self.queue.push(ajaxRequest);
+	};
+	
+	this.emit = function() {
+		if (self.queue.length > 0) {
+			var request = self.queue[0];
+			self.queue.shift();
+			$.ajax(request).done(request.onDone ? request.onDone : null).fail(request.onFail ? request.onFail : null).then(self.emit);
+		}
+	};
+	
+	return this;
+};
+
+var ajaxRequest = function(url, method, data, onDone, onFail) {
+	this.url = url;
+	this.method = method;
+	if (data) {
+		this.data = data;
+	}
+	
+	if (onDone && typeof(onDone) === 'function') {
+		this.onDone = onDone;
+	}
+	
+	if (onFail && typeof(onFail) === 'function') {
+		this.onFail = onFail;
+	}
+	
+	return this;
 };;/**
  * @file get_gutters.js
  *

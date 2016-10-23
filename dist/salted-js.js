00a0fbcb40795465ab8b65e767bed645816993eb
Leo Chen <leo@saltedherring.com>
2016-10-22 23:31:22 -1200

    gmap onboard

--- dist/salted-js.js
+++ dist/salted-js.js
@@ -235,7 +235,40 @@ var ajaxRequest = function(url, method, data, onDone, onFail) {
 		return el.margin(dimension) + el.padding(diemension);
 	};
 	
- })(jQuery);;/**
+ })(jQuery);;var gmap = function(api_key, map_id, locs) {
+	var self			=	this,
+		map				=	null,
+		center_point	=	null;
+	if (!window.google) {
+		$.when(
+			$.getScript( "https://maps.googleapis.com/maps/api/js?key=" + api_key ),
+			$.Deferred(function( deferred ){
+				$( deferred.resolve );
+			})
+		).done(self.init);
+	} else {
+		this.init();
+	}
+	
+	this.init = function() {
+		if (locs && locs.length > 0) {
+			center_point = locs[0];
+			map = new google.maps.Map(document.getElementById(map_id), {
+			  zoom: 18,
+			  center: center_point
+			});
+			for (var i = 0; i < locs.length; i++) {
+				var marker = new google.maps.Marker({
+					position: locs[i],
+					map: map
+				});
+			}
+		}
+	};
+	
+	return this;
+};
+;/**
  * @file is_above_viewport.js
  *
  * test an element is above the viewport

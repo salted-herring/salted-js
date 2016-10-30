fe8357960bfc6c57aa63b6864c03826b83e8d727
Leo Chen <leo@saltedherring.com>
2016-10-30 10:04:53 -1200

    grunt tasks

--- dist/salted-js.js
+++ dist/salted-js.js
@@ -146,7 +146,45 @@ var ajaxRequest = function(url, method, data, onDone, onFail) {
 	}
 	
 	return this;
-};;var autoAddress = function(api_key, callback) {
+};;/**
+ * - callbacks: null | {
+        onstart: function | null,
+        sucess: function | null,
+        fail: function | null,
+        done: function | null
+     }
+ * */
+(function($) {
+	$.fn.ajaxSubmit = function(cbf) {
+        var self            =   $(this),
+            endpoint        =   $(this).attr('action'),
+            method          =   $(this).attr('method'),
+            callbacks       =   $.extend({
+                                    onstart: function() {},
+                                    success: function(response) {},
+                                    fail: function(response) {},
+                                    done: function(response) {}
+                                }, cbf);
+
+        $(this).submit(function(e){
+            e.preventDefault();
+            var formData    =   new FormData($(this)[0]);
+            callbacks.onstart();
+            $.ajax({
+                url: endpoint,
+                type: method,
+                data: formData,
+                cache: false,
+                contentType: false,
+                processData: false,
+                success: callbacks.success,
+                error: callbacks.fail,
+                done: callbacks.done
+            });
+        });
+	};
+ })(jQuery);
+;var autoAddress = function(api_key, callback) {
 	var self = this;
 	this.inputField = null;
 	this.init = function() {

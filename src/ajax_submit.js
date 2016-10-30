36daeed66075ff567ecb5e9fc28ec92fa44b1e9b
Leo Chen <leo@saltedherring.com>
2016-10-31 11:03:39 +1300

    ajax_submit onboard

---
+++ src/ajax_submit.js
@@ -0,0 +1,38 @@
+/**
+ * - callbacks: null | {
+        onstart: function | null,
+        sucess: function | null,
+        fail: function | null,
+        done: function | null
+     }
+ * */
+(function($) {
+	$.fn.ajaxSubmit = function(callbacks) {
+        var self            =   $(this),
+            endpoint        =   $(this).attr('action'),
+            method          =   $(this).attr('method'),
+            callbacks       =   $.extend({
+                                    onstart: function() {},
+                                    success: function(response) {},
+                                    fail: function(response) {},
+                                    done: function(response) {}
+                                }, callbacks);
+
+        $(this).submit(function(e){
+            e.preventDefault();
+            var formData    =   new FormData($(this)[0]);
+            callbacks.onstart();
+            $.ajax({
+                url: endpoint,
+                type: method,
+                data: formData,
+                cache: false,
+                contentType: false,
+                processData: false,
+                success: callbacks.success,
+                error: callbacks.fail,
+                done: callbacks.done
+            });
+        });
+	};
+ })(jQuery);

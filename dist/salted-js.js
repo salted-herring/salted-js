ac4473facfe5762013b62e88f516800d5906a30d
Leo Chen <leo@saltedherring.com>
2016-12-02 21:51:36 -1200

    gmap added

--- dist/salted-js.js
+++ dist/salted-js.js
@@ -408,16 +408,26 @@ var ajaxRequest = function(url, method, data, onDone, onFail) {
 		return el.margin(dimension) + el.padding(diemension);
 	};
 	
- })(jQuery);;var gmap = function(api_key, map_id, locs) {
+ })(jQuery);;/*
+routing_options = {
+    enabled,
+    input_id,
+    output_id
+}
+*/
+var gmap = function(api_key, map_id, locs, zoom_rate, routing_options) {
 	var self			=	this,
 		map				=	null,
-		center_point	=	null;
-	
-	this.init = function() {
+		center_point	=	null,
+        dir_service     =   null,
+        dir_display     =   null;
+
+	this.init = function()
+    {
 		if (locs && locs.length > 0) {
 			center_point = locs[0];
 			map = new google.maps.Map(document.getElementById(map_id), {
-			  zoom: 18,
+			  zoom: zoom_rate ? zoom_rate : 18,
 			  center: center_point
 			});
 			for (var i = 0; i < locs.length; i++) {
@@ -426,12 +436,46 @@ var ajaxRequest = function(url, method, data, onDone, onFail) {
 					map: map
 				});
 			}
+
+            if (routing_options && routing_options.enabled) {
+                dir_service = new google.maps.DirectionsService();
+                dir_display = new google.maps.DirectionsRenderer();
+                dir_display.setMap(map);
+                if (routing_options.output_id) {
+                    dir_display.setPanel(document.getElementById(routing_options.output_id));
+                }
+
+                var autoComplete = new google.maps.places.Autocomplete(
+        			/** @type {!HTMLInputElement} */(document.getElementById(routing_options.input_id)),
+        			{types: ['geocode']});
+                autoComplete.addListener('place_changed', function()
+                {
+                    var place = autoComplete.getPlace();
+                    $('#' + routing_options.input_id).data('lat', place.geometry.location.lat());
+                    $('#' + routing_options.input_id).data('lng', place.geometry.location.lng());
+                });
+            }
 		}
 	};
-	
+
+    this.route = function(origin, destination, travel_mode)
+    {
+        var request = {
+            origin:         origin,
+            destination:    destination,
+            travelMode:     travel_mode ? travel_mode : 'DRIVING'
+        };
+
+        dir_service.route(request, function(response, status) {
+            if (status == 'OK') {
+                dir_display.setDirections(response);
+            }
+        });
+    };
+
 	if (!window.google) {
 		$.when(
-			$.getScript( "https://maps.googleapis.com/maps/api/js?key=" + api_key ),
+			$.getScript( "https://maps.googleapis.com/maps/api/js?key=" + api_key + "&libraries=places"),
 			$.Deferred(function( deferred ){
 				$( deferred.resolve );
 			})
@@ -439,9 +483,9 @@ var ajaxRequest = function(url, method, data, onDone, onFail) {
 	} else {
 		self.init();
 	}
-	
 	return this;
-};;/**
+};
+;/**
  * @file is_above_viewport.js
  *
  * test an element is above the viewport
--- src/gmap.js
+++ src/gmap.js
@@ -1,13 +1,23 @@
-var gmap = function(api_key, map_id, locs) {
+/*
+routing_options = {
+    enabled,
+    input_id,
+    output_id
+}
+*/
+var gmap = function(api_key, map_id, locs, zoom_rate, routing_options) {
 	var self			=	this,
 		map				=	null,
-		center_point	=	null;
-	
-	this.init = function() {
+		center_point	=	null,
+        dir_service     =   null,
+        dir_display     =   null;
+
+	this.init = function()
+    {
 		if (locs && locs.length > 0) {
 			center_point = locs[0];
 			map = new google.maps.Map(document.getElementById(map_id), {
-			  zoom: 18,
+			  zoom: zoom_rate ? zoom_rate : 18,
 			  center: center_point
 			});
 			for (var i = 0; i < locs.length; i++) {
@@ -16,12 +26,46 @@ var gmap = function(api_key, map_id, locs) {
 					map: map
 				});
 			}
+
+            if (routing_options && routing_options.enabled) {
+                dir_service = new google.maps.DirectionsService();
+                dir_display = new google.maps.DirectionsRenderer();
+                dir_display.setMap(map);
+                if (routing_options.output_id) {
+                    dir_display.setPanel(document.getElementById(routing_options.output_id));
+                }
+
+                var autoComplete = new google.maps.places.Autocomplete(
+        			/** @type {!HTMLInputElement} */(document.getElementById(routing_options.input_id)),
+        			{types: ['geocode']});
+                autoComplete.addListener('place_changed', function()
+                {
+                    var place = autoComplete.getPlace();
+                    $('#' + routing_options.input_id).data('lat', place.geometry.location.lat());
+                    $('#' + routing_options.input_id).data('lng', place.geometry.location.lng());
+                });
+            }
 		}
 	};
-	
+
+    this.route = function(origin, destination, travel_mode)
+    {
+        var request = {
+            origin:         origin,
+            destination:    destination,
+            travelMode:     travel_mode ? travel_mode : 'DRIVING'
+        };
+
+        dir_service.route(request, function(response, status) {
+            if (status == 'OK') {
+                dir_display.setDirections(response);
+            }
+        });
+    };
+
 	if (!window.google) {
 		$.when(
-			$.getScript( "https://maps.googleapis.com/maps/api/js?key=" + api_key ),
+			$.getScript( "https://maps.googleapis.com/maps/api/js?key=" + api_key + "&libraries=places"),
 			$.Deferred(function( deferred ){
 				$( deferred.resolve );
 			})
@@ -29,6 +73,5 @@ var gmap = function(api_key, map_id, locs) {
 	} else {
 		self.init();
 	}
-	
 	return this;
 };

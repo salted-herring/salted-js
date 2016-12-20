7e556d8e4ad04f2c388f574fa93c28caf221d569
Leo Chen <leo@saltedherring.com>
2016-12-20 08:04:32 -1200

    add update function to simplayer

--- src/simple_overlay.js
+++ src/simple_overlay.js
@@ -1,13 +1,13 @@
 var simplayer					=	function(title,content,buttons,zindex,maxWidth,touchClose) {
 	this.tray					=	$('<div />').attr('id','simplayer-tray');
 	this.title					=	$('<h2 />').addClass('simplayer-title').html(title);
-	this.content					=	$('<div />').addClass('simplayer-content').html(content);
-	this.buttons					=	$('<div />').addClass('clearfix simplayer-buttons');
+	this.content				=	$('<div />').addClass('simplayer-content').html(content);
+	this.buttons				=	$('<div />').addClass('clearfix simplayer-buttons');
 	this.afterClose				=	function() {
 										_wrapper.remove();
 										_tray.remove();
 										$('body').removeAttr('style');
-										
+
 										delete _self.tray;
 										delete _self.title;
 										delete _self.content;
@@ -25,8 +25,8 @@ var simplayer					=	function(title,content,buttons,zindex,maxWidth,touchClose) {
 										TweenMax.to(_wrapper, _duration, {opacity: 1, scale: 1, ease: Back.easeOut.config(1.7)});
 									}},
 		_rtween					=	{opacity: 0, scale: 0, ease: Back.easeIn.config(1.7), onComplete: _self.afterClose};
-	
-	
+
+
 	if (buttons && typeof(buttons) == 'object' && buttons.length > 0) {
 		buttons.forEach(function(btn) {
 			var sbtn = $('<button />').addClass('simplayer-button button').html(btn.Label);
@@ -42,19 +42,19 @@ var simplayer					=	function(title,content,buttons,zindex,maxWidth,touchClose) {
         });
 		_thisButtons.append(sbtn);
 	}
-	
+
 	this.wrapper				=	$('<div />').attr('id', 'simplayer-wrapper').append(this.title, this.content, this.buttons);
-	
+
 	if (_maxWidth) {
 		this.wrapper.css('max-width', _maxWidth);
 	}
-	
+
 	_wrapper = this.wrapper;
-	
+
 	zindex = zindex ? zindex : 99998;
 	this.tray.css('z-index', zindex);
 	this.wrapper.css('z-index', zindex+1);
-	
+
 	this.show 			=	function(effect) {
 		$('body').css('overflow','hidden').append(_tray, _wrapper);
 		_wrapper.css('max-height', '90%');
@@ -62,9 +62,9 @@ var simplayer					=	function(title,content,buttons,zindex,maxWidth,touchClose) {
 			wrapperPadding		=	_wrapper.css('padding-top').replace(/px/gi, '').toFloat() + _wrapper.css('padding-bottom').replace(/px/gi, '').toFloat(),
 			margins				=	_wrapper.find('.simplayer-title').margin('vertical') + _wrapper.find('.simplayer-buttons').margin('vertical'),
 			nonCntHeight		=	_wrapper.find('.simplayer-title').outerHeight() + _wrapper.find('.simplayer-buttons').outerHeight();
-		
+
 		_wrapper.find('.simplayer-content').css('max-height', wrapperHeight - nonCntHeight - margins - wrapperPadding).css('overflow-y','auto');
-		
+
 		if (effect) {
 			_duration			=	effect.duration;
 			_tween				=	clone(effect.from);
@@ -74,31 +74,39 @@ var simplayer					=	function(title,content,buttons,zindex,maxWidth,touchClose) {
 			_rtween				=	clone(effect.from);
 			_rtween.onComplete	=	_self.afterClose;
 		}
-		
+
 		if (_touchClose) {
 			_tray.unbind('mousedown').mousedown(function() { _self.close(); });
 		}
-		
+
 		TweenMax.to(_wrapper, 0, _tween);
 		TweenMax.to(_tray, 0, {opacity: 0, onComplete: function() {
 			TweenMax.to(_tray, _duration, {opacity: 1});
 		}});
 	};
-	
-	
+
+	this.update					=	function(title, content, callback) {
+		self.title.html(title);
+		self.content.html(content);
+		if (callback) {
+			callback();
+		}
+	};
+
+
 	this.close					=	function() {
 		TweenMax.to(_tray, _duration, {opacity: 0});
 		TweenMax.to(_wrapper, _duration, _rtween);
 	};
-	
+
 	this.btnEvent				=	function(idx, event) {
 		var buttons = this.buttons.find('.simplayer-button');
 		if (idx < buttons.length) {
 			buttons.eq(idx).unbind('click').click(event);
 		}
 	};
-	
-	
+
+
 };
 function HijackAlert(tweenObjects) {
 	window.alert = function(msg, title) {

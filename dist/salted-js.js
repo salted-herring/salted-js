113f68fa3bf1a225b137d9ec7e2c2e9dd1c936dd
Leo Chen <leo@saltedherring.com>
2017-05-04 21:11:10 -1200

    gmap jQuery plugin

--- dist/salted-js.js
+++ dist/salted-js.js
@@ -623,6 +623,24 @@ var gmap = function(api_key, map_id, locs, zoom_rate, routing_options, disableUI
 	}
 	return this;
 };
+
+(function($)
+{
+	$.fn.gmap = function(cbf)
+    {
+        var self        =   $(this),
+            callback    =   cbf,
+            lat         =   self.data('lat').toFloat(),
+            lng         =   self.data('lng').toFloat(),
+            zoom        =   Math.round(self.data('zoom').toFloat()),
+            api         =   self.data('api'),
+            input       =   self.data('input'),
+            output      =   self.data('output'),
+            map         =   new gmap(api, self.attr('id'), [{lat: lat, lng: lng}], zoom, {enabled: true, input_id: input, output_id: output});
+
+        return map;
+    };
+ })(jQuery);
 ;/**
  * @file is_above_viewport.js
  *

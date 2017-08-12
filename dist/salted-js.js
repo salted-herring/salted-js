e357d3c05cf043b56ef06cef5e246648efb9a744
Leo Chen <leo@saltedherring.com>
2017-08-11 12:02:05 -1200

    bulification added

--- dist/salted-js.js
+++ dist/salted-js.js
@@ -390,6 +390,49 @@ var autoAddress = function(api_key, callback) {
 
 	return this.init();
 };
+;if (typeof Handlebars) {
+    var BulificationTemplate    =   Handlebars.compile('<div class="message is-{{#if Colour}}{{Colour}}{{else}}dark{{/if}} bulification"><div class="message-header"><p>{{Title}}</p><button class="delete" aria-label="delete"></button></div><div class="message-body">{{Content}}<div class="has-text-centered actions"><a class="button is-info">OK</a></div></div></div>'),
+        Bulification            =   function(title, content, colour)
+        {
+            this.html           =   $(BulificationTemplate({Title: title, Content: content, Colour: colour}));
+
+            this.show           =   function()
+                                    {
+                                        $('body').addClass('blurred');
+                                        me.html.appendTo($('body'));
+                                    };
+
+            this.close          =   function(e)
+                                    {
+                                        if (e) {
+                                            e.preventDefault();
+                                            me.html.remove();
+                                            $('body').removeClass('blurred');
+                                        }
+                                    };
+
+            var me              =   this;
+
+            this.html.find('button.delete').click(this.close);
+            this.html.find('a.button').click(this.close);
+
+            return this;
+        };
+}
+
+function BulmaAlert()
+{
+    if (!(typeof Handlebars)) {
+        console.warn('To use Bulification, install http://handlebarsjs.com/ first!');
+    }
+
+    window.alert        =   function(msg, title)
+    {
+        if (!title) { title = 'Message'; }
+        var msgbox = new Bulification(title, msg);
+        msgbox.show();
+    };
+}
 ;(function($) {
 	$.fn.fixy = function(prTopOffset) {
         var me              =   $(this),

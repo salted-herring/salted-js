bd7959d401dc480986cc12acd6900f9bf6f4fc19
Leo Chen <leo@saltedherring.com>
2018-05-20 23:17:37 -1200

    using axios

--- src/ajax_submit.js
+++ src/ajax_submit.js
@@ -7,45 +7,65 @@
      }
  * */
 (function($) {
-	$.fn.ajaxSubmit = function(cbf) {
+    $.fn.ajaxSubmit         =   function(cbf)
+    {
         var self            =   $(this),
             endpoint        =   $(this).attr('action'),
             method          =   $(this).attr('method'),
-			lockdown		=	false,
+            lockdown        =   false,
             callbacks       =   $.extend({
-									validator: function() { return true; },
+                                    validator: function() { return true; },
                                     onstart: function() {},
                                     success: function(response) {},
                                     fail: function(response) {},
                                     done: function(response) {}
                                 }, cbf);
 
-        $(this).submit(function(e){
+        $(this).submit(function(e)
+        {
             e.preventDefault();
-			e.stopPropagation();
-			
-			if (!callbacks.validator()) {
-				return false;
-			}
+            e.stopPropagation();
 
-			if (lockdown) {
-				return false;
-			}
+            if (!callbacks.validator()) {
+                return false;
+            }
 
-			lockdown = true;
-            var formData    =   new FormData($(this)[0]);
+            if (lockdown) {
+                return false;
+            }
+
+            lockdown            =   true;
+
+            var formData        =   new FormData($(this)[0]);
+
             callbacks.onstart();
-            $.ajax({
-                url: endpoint,
-                type: method,
-                data: formData,
-                cache: false,
-                contentType: false,
-                processData: false
-            }).done(callbacks.success).fail(callbacks.fail).always(function(response) {
-				lockdown = false;
-				callbacks.done(response);
-			});
+
+            if (axios) {
+                console.log('using axios');
+                axios.post(endpoint, formData).then(function(response)
+                {
+                    lockdown    =   false;
+                    callbacks.success(response.data);
+                }).catch(function (error)
+                {
+                    lockdown    =   false;
+                    callbacks.fail(error.data);
+                });
+            } else {
+                console.log('using ajax');
+                $.ajax({
+                    url         :   endpoint,
+                    type        :   method,
+                    data        :   formData,
+                    cache       :   false,
+                    contentType :   false,
+                    processData :   false
+                }).done(callbacks.success).fail(callbacks.fail).always(function(response)
+                {
+                    lockdown = false;
+                    callbacks.done(response);
+                });
+            }
         });
-	};
+    };
  })(jQuery);
